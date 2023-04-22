0496672b323219ff25efb2d9e6e57842ee84d348
mitulchy07 <mitul9616@gmail.com>
2023-04-22 03:03:01 +0200

    service.ts added

--- src/app/data/passwords.service.ts
+++ src/app/data/passwords.service.ts
@@ -9,15 +9,15 @@ export class PasswordsService {
   constructor(private _http: HttpClient ) {}
   
   addingData(data: any): Observable<any>{
-    return this._http.post('/passwords', data)
+    return this._http.post('https://password-manager-git-main-mitulchy07.vercel.app//passwords', data)
   }
   updatingData(id: number, data: any): Observable<any>{
-    return this._http.put(`/passwords/${id}`, data)
+    return this._http.put(`https://password-manager-git-main-mitulchy07.vercel.app//passwords/${id}`, data)
   }
   gettingData(): Observable<any>{
-    return this._http.get('/passwords')
+    return this._http.get('https://password-manager-git-main-mitulchy07.vercel.app//passwords')
   }
   deleteData(id:number): Observable<any>{
-    return this._http.delete(`/passwords/${id}`);
+    return this._http.delete(`https://password-manager-git-main-mitulchy07.vercel.app//passwords/${id}`);
   }
 }
