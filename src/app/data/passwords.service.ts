ef1454b1abac776b965299127969cfe0f1e8a96a
mitulchy07 <mitul9616@gmail.com>
2023-04-22 03:22:42 +0200

    Put operation updated

--- src/app/data/passwords.service.ts
+++ src/app/data/passwords.service.ts
@@ -12,7 +12,7 @@ export class PasswordsService {
     return this._http.post('http://localhost:3000/passwords', data)
   }
   updatingData(id: number, data: any): Observable<any>{
-    return this._http.put(`http://localhost:3000//passwords/${id}`, data)
+    return this._http.put(`http://localhost:3000/passwords/${id}`, data)
   }
   gettingData(): Observable<any>{
     return this._http.get('http://localhost:3000/passwords')
